52279153f166e515e40c72511ee371ca2981f80d
Thiago <thiago.pereira.moreira@gmail.com>
2024-01-26 01:29:22 -0300

    final dashboard

--- app/dashboard/customers/page.tsx
+++ app/dashboard/customers/page.tsx
@@ -1,3 +1,9 @@
-  export default function Page() {
+import { Metadata } from 'next';
+ 
+export const metadata: Metadata = {
+  title: 'Customers',
+};
+
+export default function Page() {
     return <p>Customers Page</p>;
   }
